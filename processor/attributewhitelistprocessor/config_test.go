4ec6ac68619158839c20b758050f1e7b3da94f9c
Chris Taormina <christopher.taormina@zocdoc.com>
2020-05-28 14:24:55 -0400

    chore: some more tests

--- processor/attributewhitelistprocessor/config_test.go
+++ processor/attributewhitelistprocessor/config_test.go
@@ -1,1 +1,42 @@
 package attributewhitelistprocessor
+
+import (
+	"github.com/open-telemetry/opentelemetry-collector/config"
+	"github.com/open-telemetry/opentelemetry-collector/config/configcheck"
+	"github.com/open-telemetry/opentelemetry-collector/config/configmodels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"path"
+	"testing"
+)
+
+func TestLoadConfig(t *testing.T) {
+	factories, err := config.ExampleComponents()
+	require.NoError(t, err)
+	factory := &Factory{}
+	factories.Processors[configmodels.Type(typeStr)] = factory
+
+	err = configcheck.ValidateConfig(factory.CreateDefaultConfig())
+	require.NoError(t, err)
+
+	config, err := config.LoadConfigFile(
+		t,
+		path.Join(".", "testdata", "testConfig.yaml"),
+		factories)
+
+	require.Nil(t, err)
+	require.NotNil(t, config)
+
+	proc := config.Processors["attributewhitelist"]
+	assert.Equal(t, proc,
+		&Config{
+			ProcessorSettings: configmodels.ProcessorSettings{
+				TypeVal: "attributewhitelist",
+				NameVal: "attributewhitelist",
+			},
+			AttributeWhiteList: []string {
+				"\\bsomething\\b",
+				"http.*",
+			},
+		})
+}
